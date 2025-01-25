10eeeb2559c3c6ae495bca6bdf7a63f8d20c7f66
Rafi Jadika <Rafi Jadika@LAPTOP-M1GUN5HA>
2025-01-26 03:38:17 +0700

    membuat model Reservasi

--- app/src/main/java/com/example/pamfinal/model/Reservasi.kt
+++ app/src/main/java/com/example/pamfinal/model/Reservasi.kt
@@ -1,10 +1,18 @@
 package com.example.pamfinal.model
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Reservasi(
-    val id_reservasi: Int,      // ID unik untuk reservasi
-    val id_villa: Int,          // ID villa yang direservasi
-    val id_pelanggan: Int,      // ID pelanggan yang melakukan reservasi
-    val Check_in: String,       // Tanggal check-in (format: "yyyy-MM-dd")
-    val Check_out: String,      // Tanggal check-out (format: "yyyy-MM-dd")
-    val Jumlah_kamar: Int       // Jumlah kamar yang dipesan
+    @SerialName("id_reservasi")
+    val idReservasi: Int,      // ID unik untuk reservasi
+    @SerialName("id_villa")
+    val idVilla: Int,          // ID villa yang direservasi
+    @SerialName("id_pelanggan")
+    val idPelanggan: Int,      // ID pelanggan yang melakukan reservasi
+    val CheckIn: String,       // Tanggal check-in (format: "yyyy-MM-dd")
+    val CheckOut: String,      // Tanggal check-out (format: "yyyy-MM-dd")
+    @SerialName("jumlah_kamar")
+    val JumlahKamar: Int       // Jumlah kamar yang dipesan
 )
