e8b4949135db25f0cf0bee0552467ca54a96f5cc
Rafi Jadika <Rafi Jadika@LAPTOP-M1GUN5HA>
2025-01-26 03:37:59 +0700

    membuat model Review

--- app/src/main/java/com/example/pamfinal/model/Review.kt
+++ app/src/main/java/com/example/pamfinal/model/Review.kt
@@ -1,8 +1,16 @@
 package com.example.pamfinal.model
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Review(
-    val id_review: Int,         // ID unik untuk review
-    val id_reservasi: Int,      // ID reservasi yang direview
-    val nilai: String,          // Nilai kepuasan (Sangat Puas, Puas, Biasa, Tidak Puas, Sangat Tidak Puas)
-    val komentar: String        // Komentar dari pelanggan
+    @SerialName("id_review")
+    val idReview: Int,
+    @SerialName("id_reservasi")// ID unik untuk review
+    val idReservasi: Int,
+    @SerialName ("nilai")// ID nilai yang direview
+    val nilaiReview: String,
+    @SerialName("komentar")// Nilai komentar (Sangat Puas, Puas, Biasa, Tidak Puas, Sangat Tidak Puas)
+    val komentarReview: String        // Komentar dari pelanggan
 )
